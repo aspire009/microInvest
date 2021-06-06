978ecfcc425251ffb3d4dbc611f3370413e084a4
Hardik Bansal <hardik.bansal@hashedin.com>
2021-06-06 12:50:35 +0530

    Sign in signup changes

--- project-saver-frontend/src/components/landingPages/SigninSignupPage/SigninSignupPage.tsx
+++ project-saver-frontend/src/components/landingPages/SigninSignupPage/SigninSignupPage.tsx
@@ -2,6 +2,7 @@ import './SigninSignupPage.css'
 import SigninSignupLeftImage from '../../../assets/images/signinSignupPage/signinSignupLeftImage.png'
 import SigninSignupPallette from './SigninSignupPallette/SigninSignupPallette'
 import { useState } from 'react';
+import LogoPurple from '../../../assets/images/logos/logoPurplewhite.png'
 
 
 const SigninSignupPage = (props: { mode: string }) => {
@@ -9,9 +10,19 @@ const SigninSignupPage = (props: { mode: string }) => {
 
     return (
         <div className="signin-signup-main">
-            <div className="signin-signup-left">
-                <img className="signin-signup-left-image" src={SigninSignupLeftImage} />
+            <div className="signin-signup-curved">
+                <div className="signin-signup-left">
+                    <div className="signin-signup-logo-wrapper">
+                        <img src={LogoPurple} className="signin-signup-logo"></img>
+                        <label className="signin-signup-logo-label">SaveEasy</label>
 
+                    </div>
+                    <img className="signin-signup-left-image" src={SigninSignupLeftImage} />
+
+                </div>
+                <div className="signin-signup-extra">
+
+                </div>
             </div>
 
             <div className="signin-signup-right">
